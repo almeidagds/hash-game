05311907a666ff65065930e2289b733ac98f38c6
Gabriel de Almeida <almeidag2001@gmail.com>
2022-06-27 20:32:13 -0300

    Starting to create board design.

--- hash-game/src/App.tsx
+++ hash-game/src/App.tsx
@@ -1,8 +1,21 @@
-import './App.css';
+import style from './App.module.scss';
 
 function App() {
   return (
     <div className="App">
+        <div className={style.board}>
+          <ul>
+            <li className={style.case}></li>
+            <li className={style.case}></li>
+            <li className={style.case}></li>
+            <li className={style.case}></li>
+            <li className={style.case}></li>
+            <li className={style.case}></li>
+            <li className={style.case}></li>
+            <li className={style.case}></li>
+            <li className={style.case}></li>
+          </ul>
+        </div>
     </div>
   );
 }
