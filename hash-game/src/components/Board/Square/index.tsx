5ac3756791b053cd5bae40149a97a218c5ec9212
Gabriel de Almeida <almeidag2001@gmail.com>
2022-07-05 23:36:01 -0300

    Refactoring components.

--- hash-game/src/components/Board/Square/index.tsx
+++ hash-game/src/components/Board/Square/index.tsx
@@ -1,34 +1,25 @@
 import style from "./Square.module.scss";
 
+export type squareOptions = "x" | "o" | null;
+
 interface Props {
-    value?: any;
-    onClick?: () => void;
+    value: squareOptions;
+    onClick: () => void;
 }
 
 export function Square({value, onClick}: Props) {
 
-    function renderCorrectValue(value: any)  {
-
-        let valueElement;
-
+    function renderValueSVG(value: any)  {
         if (value === "x") {
-            valueElement = <svg className={style.cross}>
-            <path className={style['cross-left-line']}></path>
-            <path className={style['cross-right-line']}></path>
-            </svg>
-            return valueElement;
+            return <svg className={style.cross}><path className={style['cross-left-line']}></path><path className={style['cross-right-line']}></path></svg>;
         } else if (value === "o") {
-            valueElement = <svg className={style.circle}>
-            <circle />
-            </svg>;
-            return valueElement;
+            return <svg className={style.circle}><circle /></svg>;
         }
-        return "";
     }
 
     return (
         <li className={style.square} onClick={onClick} >
-            {renderCorrectValue(value)}
+            {renderValueSVG(value)}
         </li>
     );
 }
--- hash-game/src/components/Board/index.tsx
+++ hash-game/src/components/Board/index.tsx
@@ -1,36 +1,73 @@
 import { useState } from "react";
 import style from "./Board.module.scss"
-import { Square } from "./Square";
+import { Square, squareOptions } from "./Square";
 
-interface Props {
-  xIsNext?: boolean;
-}
 export function Board() {
 
-  let [board, setBoard] = useState({square: Array(9).fill(null), xIsNext: true});
-  function setSquare(position: any) {
-    if (!board.square[position]) {
-      let newBoard = board.square;
-      newBoard[position] = board.xIsNext ? "x" : "o";
-      setBoard({square: newBoard, xIsNext: !board.xIsNext});
-    }
-    const isThereMoreSpace = board.square.some((square) => !square);
-    if (!isThereMoreSpace) {
-      setBoard({square: Array(9).fill(null), xIsNext: true});
+  let [board, setBoard] = useState({
+    squares: Array(9).fill(null),
+    xIsNext: true
+  });
+
+  function setSquareValue(index: number) {
+
+    if (!board.squares[index]) {
+
+      const squares = board.squares;
+      squares[index] = board.xIsNext ? "x" : "o";
+
+      setBoard({
+        squares: squares, 
+        xIsNext: !board.xIsNext
+      });
+
+      const winner = calculateWinner(squares);
+
+      if (winner) {
+        console.log("Winner: " + winner);
+        setBoard({
+          squares: Array(9).fill(null),
+          xIsNext: true
+        });
+      }
     }
   }
+
+  function calculateWinner(squares: squareOptions[]): squareOptions | void {
+    const winnerLines: Array<number[]> = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    let winner;
+    winnerLines.forEach((winnerLine) => {
+      const [a, b, c] = winnerLine;
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        winner = squares[a];
+      }
+    });
+
+    return winner;
+  }
+  
   return (
       <div className={style.board}>
         <ul>
-          <Square value={board.square[0]} onClick={() => setSquare(0)} />
-          <Square value={board.square[1]} onClick={() => setSquare(1)} />
-          <Square value={board.square[2]} onClick={() => setSquare(2)} />
-          <Square value={board.square[3]} onClick={() => setSquare(3)} />
-          <Square value={board.square[4]} onClick={() => setSquare(4)} />
-          <Square value={board.square[5]} onClick={() => setSquare(5)} />
-          <Square value={board.square[6]} onClick={() => setSquare(6)} />
-          <Square value={board.square[7]} onClick={() => setSquare(7)} />
-          <Square value={board.square[8]} onClick={() => setSquare(8)} />
+          <Square value={board.squares[0]} onClick={() => setSquareValue(0)} />
+          <Square value={board.squares[1]} onClick={() => setSquareValue(1)} />
+          <Square value={board.squares[2]} onClick={() => setSquareValue(2)} />
+          <Square value={board.squares[3]} onClick={() => setSquareValue(3)} />
+          <Square value={board.squares[4]} onClick={() => setSquareValue(4)} />
+          <Square value={board.squares[5]} onClick={() => setSquareValue(5)} />
+          <Square value={board.squares[6]} onClick={() => setSquareValue(6)} />
+          <Square value={board.squares[7]} onClick={() => setSquareValue(7)} />
+          <Square value={board.squares[8]} onClick={() => setSquareValue(8)} />
         </ul>
       </div>
   );
