ccb06e799851b1317e3614faaeafb4dbb4063200
Gabriel de Almeida <almeidag2001@gmail.com>
2022-07-08 20:54:11 -0300

    Creating modal buttons.

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -10,11 +10,19 @@ export function Modal({title, body, buttons}: Props) {
 
     return (
         <div className={style.modal}>
-            <h1 className={style.title}>E o vencedor é...</h1>
-            <div className={style.body}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
-
+            <header className={style.title}>
+                <h1>And the winner is...</h1>
+                <span className={style["close-button"]}>
+                    X
+                </span>
+            </header>
+            <div className={style.body}>
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.
             </div>
-            <div className={style.footer}>botão 1 e botão 2</div>
+            <div className={style.footer}>
+                <button className={style.button}>Play again</button>
+                <button className={style.button}>Close</button>
+            </div>
         </div>
     );
 }
