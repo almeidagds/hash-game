f6e970410c90e84d5f1f3e025054728e890a53ca
Gabriel de Almeida <almeidag2001@gmail.com>
2022-07-08 20:11:34 -0300

    First style of the modal component.

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,14 +1,20 @@
 import style from "./Modal.module.scss";
 
 interface Props {
-    message: string;
+    title?: string;
+    body?: string;
+    buttons?: string;
 }
 
-export function Modal() {
+export function Modal({title, body, buttons}: Props) {
 
     return (
         <div className={style.modal}>
-            <h1>teste</h1>
+            <h1 className={style.title}>E o vencedor é...</h1>
+            <div className={style.body}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+
+            </div>
+            <div className={style.footer}>botão 1 e botão 2</div>
         </div>
     );
 }
