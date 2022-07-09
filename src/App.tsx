c376be4d1112c423fd14a5569df1d2e0f8f5893b
Gabriel de Almeida <almeidag2001@gmail.com>
2022-07-09 15:41:55 -0300

    Adding a close modal function to the modal component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import style from './App.module.scss';
 import { Board } from './components/Board';
 import { Modal } from './components/Modal'
+import { v4 as uuidv4 } from 'uuid';
+
 function App() {
   return (
     <div className={style.App}>
       <Board />
-      <Modal />
+      <Modal id={uuidv4()}
+             title="And the winner is..."
+             body="Lorem ipsum testando um corpo qualquer aqui nesse espaço." />
     </div>
   );
 }
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,23 +1,29 @@
 import style from "./Modal.module.scss";
+import $ from "jquery";
 
 interface Props {
-    title?: string;
-    body?: string;
+    id: string;
+    title: string;
+    body: string;
     buttons?: string;
 }
 
-export function Modal({title, body, buttons}: Props) {
+export function Modal({id, title, body, buttons}: Props) {
+
+    function closeModal(id: string): void {
+        $(`#${id}`).css("display", "none");
+    }
 
     return (
-        <div className={style.modal}>
+        <div id={id} className={style.modal}>
             <header className={style.title}>
-                <h1>And the winner is...</h1>
-                <span className={style["close-button"]}>
+                <h1>{title}</h1>
+                <span className={style["close-button"]} onClick={() => closeModal(id)}>
                     X
                 </span>
             </header>
             <div className={style.body}>
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.
+                {body}
             </div>
             <div className={style.footer}>
                 <button className={style.button}>Play again</button>
