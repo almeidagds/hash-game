5797cb0637b1a6a3cdf1cda12119243bd3dec279
Gabriel de Almeida <almeidag2001@gmail.com>
2022-10-13 17:41:56 -0300

    Making changes on the board menu.

--- src/components/Board/Menu/index.tsx
+++ src/components/Board/Menu/index.tsx
@@ -1,20 +1,55 @@
+import { useState } from "react";
 import { GameDifficulties } from "../../../enums/game-difficulties";
 import styles from "./Menu.module.scss";
 
 export function Menu() {
+    let [players, setPlayers] = useState(1);
+
     return (
         <div className={styles.menu}>
-            <label>
-                Difficulty: 
-                <select name="difficulty" className={styles["menu__difficulty"]}>
+            <span className={styles["menu__game-mode"]}>
+
+                <input type="radio" 
+                    id="one-player" 
+                    name="game-mode" 
+                    value="1" 
+                    className={styles["menu__game-mode__input"]} />
+                <label htmlFor="one-player" 
+                    className={styles["menu__game-mode__label"]} 
+                    onClick={() => setPlayers(1)}>
+                    1 player
+                </label>
+
+                <input type="radio" 
+                    id="two-players" 
+                    name="game-mode" 
+                    value="2 players" 
+                    className={styles["menu__game-mode__input"]} />
+                <label htmlFor="two-players"
+                    className={styles["menu__game-mode__label"]} 
+                    onClick={() => setPlayers(2)}>
+                    2 players
+                </label>
+           
+            </span>
+
+            <span className={styles["menu__difficulty"]}>
+                <label htmlFor="difficulty" 
+                    className={styles["menu__difficulty__label"]}>
+                    Difficulty: 
+                </label>
+                <select id="difficulty" 
+                    name="difficulty" 
+                    className={styles["menu__difficulty__input"]}
+                    disabled={players !== 1}>
                     {
                         Object.keys(GameDifficulties).map((difficulty: string, index) => {
                             const difficultyNameWithFirstLetterInUpper = difficulty.charAt(0).toLocaleUpperCase() + difficulty.slice(1);
                             return <option key={index} value={difficulty}>{difficultyNameWithFirstLetterInUpper}</option>
                         })
                     }
-                </select>
-            </label>
+                </select> 
+            </span>
         </div>
     );
 }
